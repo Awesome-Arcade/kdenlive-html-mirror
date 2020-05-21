c9f225b776dbbed2059bea556c2155898c858487
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2020-05-21 20:09:21 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function 
+  (req, res) {
+  res.write('Hello World!'); //write a response to the 
+  client
+  res.end(); //end the response
+}).listen($PORT);
